048f2e695c5f147f60e5e19e4dc40b3357d270de
Kestutis Burba <kestutis@brandgate.com>
2018-01-02 23:57:09 +0200

    Adding ExpensesSummary component

---
+++ src/tests/selectors/expenses-total.test.js
@@ -0,0 +1,18 @@
+import moment from 'moment';
+import selectExpensesTotal from '../../selectors/expenses-total';
+import expenses from '../fixtures/expenses';
+
+test('should return 0 if no expenses', () => {
+    const res = selectExpensesTotal([]);
+    expect(res).toBe(0);
+})
+
+test('should correctly add up one expense', () => {
+    const res = selectExpensesTotal([expenses[1]]);
+    expect(res).toBe(109500);
+})
+
+test('should correct add up multiple expenses', () => {
+    const res = selectExpensesTotal(expenses);
+    expect(res).toBe(114195);
+})
