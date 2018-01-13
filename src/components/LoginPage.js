400dd6a4ff09109cd44d883e3b437154b8a6732c
Kestutis Burba <kburba@gmail.com>
2018-01-14 01:15:34 +0200

    Add Expensify styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Log in</button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Expensify app</h1>
+            <p>Please log in</p>
+            <button onClick={startLogin} className="box-layout__button">Log in with Google</button>
+        </div>
     </div>
 );
 
